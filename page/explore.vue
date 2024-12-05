ff70c6d8d1d6a506a55173c201a3740fa0b15c92
yji35j42012 <yji35j42012@gmail.com>
2024-12-05 18:50:50 +0800

    上傳item.picSrc

--- page/explore.vue
+++ page/explore.vue
@@ -9,7 +9,7 @@
 				<div class="content_item_detail">{{ item.detail }}</div>
 			</div>
 			<div class="content_item_pic" v-if="item.picSrc">
-				<img :src="item.picSrc" alt="">
+				<img :src="require(item.picSrc)" alt="">
 			</div>
 			<div class="content_item_foot">
 				<ul class="tag">
